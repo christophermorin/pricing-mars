4f48d1acb240f7251bf8fbc08f7b870b263dcf11
Chris Morin <longhumansbiz@gmail.com>
2023-05-27 20:29:18 -0300

    setting up

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import DropDown from './components/Dropdown'
+import DesktopNav from './components/NavBar/DesktopNav'
+
+function App() {
+  return (
+    <div>
+      <DesktopNav />
+
+      {/* <nav>
+        <div className='large-container'>
+          non intere
+          <DropDown comp={<div className='beep'>test</div>} />
+          <DropDown comp={<div>Second test</div>} />
+        </div>
+      </nav> */}
+    </div>
+
+  )
+}
+
+export default App
